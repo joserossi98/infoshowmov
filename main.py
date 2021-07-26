524b777708afdf9c5ac5fe8cd299bf222f95f8e2
Carlos Rossi <carlos.rossi16@gmail.com>
2021-07-25 21:38:01 -0400

    asldnasd

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import os
 import tmdbsimple as tmdb
+
+tmdb.API_KEY = ''
+
+#escoger lenguaje 
+# input 1 para espanol y 2 para ingles
+lenguaje = int(input("1 para espanol y 2 para ingles"))
+
+dictEspanol = {
+  "bienvenido": "bienvenido al app que bucas peliculas!",
+  "model": "Mustang",
+  "year": 1964
+}
+
+dictIngles = {
+  "bienvenido": "Welcome to my app",
+  "model": "Mustang",
+  "year": 1964
+}
+
+
+while True:
+    os.system("cls");
+
+    print("""bienvenido a infoshowmov
+    [0] Buscar una pelicula/serie/actor/actriz
+    [1] mostrar top 10 peliculas 
+    [2] mostrar top 10 series
+    """);
+    opcion = input("dime una opcion");
+    os.system("pause");
+
+        
